86a3b4107e771230f23d41cd34302d6908fdcfa8
Fadith Escorcia <feescorciam@protonmail.com>
2020-07-02 03:51:24 -0500

    commit

---
+++ src/data/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  defaultTitle: 'Fad!',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Fadith Escorcia',
+  url: '',
+  legalName: 'Fadith Escorcia',
+  defaultDescription: 'I’m Fadith and I’m a Frontend Developer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/',
+    github: 'https://github.com/FAD95',
+    linkedin: 'https://www.linkedin.com/in/fadesc/',
+    instagram: 'https://www.instagram.com/neofad/',
+    youtube: 'https://www.youtube.com',
+  },
+  googleAnalyticsID: '',
+  themeColor: '#900707',
+  backgroundColor: '#900707',
+  social: {
+    facebook: 'appId',
+    twitter: '',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2020',
+};
